5559fb9ad3ae591b2fd5c4b607bf5cdc720b435b
carlosbelop <belopcarlos@uma.es>
2023-11-30 10:12:40 +0100

    Descompresión y filtrado

--- code/load.sh
+++ code/load.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Realizar un script para descargar el archivo 9606.protein.links.v12.0.txt desde stringdb
-# ----aquí o incluir el código en get_51_gene_network.py
+# Descompresión del archivo 9606.protein.links.v12.0.txt.zip
+unzip ./9606.protein.links.v12.0.zip
+
+# Filtrado del archivo 9606.protein.links.v12.0.txt
+awk -F" " '$3 > 800 {print $0}' 9606.protein.links.v12.0.txt > 9606.protein.links.v12.0.txt
 
 # Execute Python script to get the 51 gene network and their ids for DIAMOnD
 python get_51_gene_network.py
 
 # From the obtained genes ids, run DIAMOnD to enlarge the network to 200 genes
-python DIAMOnD.py 9606.protein.links.v12.0.txt grafo_51_genes.csv 200 propaged_genes.txt
+python DIAMOnD.py 9606.protein.links.v12.0.txt grafo_51_genes.txt 200 propaged_genes.txt
 
 # Get a network with the 200 genes
 python get_200_gene_network.py
